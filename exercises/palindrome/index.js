8761b02f0dbf3a3638ac491417e8e0f4d63cb98d
ryantorecampo <ryan.torrecampo27@gmail.com>
2019-10-07 23:40:04 -0500

    finished up to palindrome

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,6 +7,23 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+// function palindrome(str) {
+//     const reversed = str.split("").reduce((prev, char) => char + prev, "");
+//     return str === reversed;
+// }
+
+// function palindrome(str) {
+//     let reversed = "";
+//     for (let char of str) {
+//         reversed = char + reversed;
+//     }
+//     return str === reversed;
+// }
+
+function palindrome(str) {
+    return str.split("").every((char, i) => {
+        return char === str[str.length - i - 1];
+    });
+}
 
 module.exports = palindrome;
--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,34 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+// function reverse(str) {
+//     let revArr = [];
+//     str.split("").forEach(item => {
+//         revArr.unshift(item);
+//     });
+//     return revArr.join("");
+// }
+
+// function reverse(str) {
+//     let reversed = "";
+//     for (let i = str.length - 1; i >= 0; i--) {
+//         reversed += str[i];
+//     }
+//     return reversed;
+// }
+
+// function reverse(str) {
+//     let reversed = "";
+
+//     for (let character of str) {
+//         reversed = character + reversed;
+//     }
+
+//     return reversed;
+// }
+
+function reverse(str) {
+    return str.split("").reduce((prev, curr) => curr + prev, "");
+}
 
 module.exports = reverse;
